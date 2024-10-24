0c1d5a806043ec894a8b6a0eb4f2dfad12abfb0b
stout0123 <kidweilenmann2@gmail.com>
2024-10-24 13:36:16 -0600

    script.js

---
+++ EH Main Menu/script.js
@@ -0,0 +1,7 @@
+function openTranslator() {
+    window.open('../EH Translator English to EH/index.html', '_blank');
+}
+
+function openTesting() {
+    window.open('../EH Testing/index.html', '_blank');
+}
